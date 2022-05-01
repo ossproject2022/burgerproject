e3c7825e38a67ce8ed3b8c50f6253da6806395fe
2000kms <rlaalstkd0@naver.com>
2022-05-02 01:43:44 +0900

    create headerfile

---
+++ manager.h
@@ -0,0 +1,7 @@
+#include "product.h"
+void saveData(Product *p,int now);
+int load(Product *p);
+int listBurger(Product *p,int now);
+int searchName(Product *p,int now);
+int searchPrice(Product *p,int now);
+int searchBurger(Product *p,int now);
---
+++ product.h
@@ -0,0 +1,12 @@
+typedef struct{
+    char name[40];
+    int price;
+    char bread[30];
+    char patty[30];
+    char sauce[30];
+}Product;
+int selectMenu();
+int createBurger(Product *p,int now);
+void readBurger(Product p);
+int updateBurger(Product *p,int now);
+int deleteBurger(Product *p,int now);
