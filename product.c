d0a03aecf5456557622d08aa1b663a6a66dcfb5e
sanha33 <mountrive@naver.com>
2022-05-06 23:02:29 +0900

    create selectMenu() function

--- product.c
+++ product.c
@@ -5,6 +5,25 @@
 #include <stdlib.h>
 #include "product.h"
 
+int selectMenu(){
+ int menu;
+ printf("\n*** Burger Project ***\n");
+ printf("1. 조회\n");
+ printf("2. 추가\n");
+ printf("3. 수정\n");
+ printf("4. 삭제\n");
+ printf("5. 파일 저장\n");
+ printf("6. 이름 검색\n");
+ printf("7. 가격 검색\n");
+ printf("8. 재료 검색\n");
+ printf("0. 종료\n\n");
+ printf("=> 원하는 메뉴는? ");
+ scanf("%d", &menu);
+ getchar();
+ printf("\n");
+ return menu;
+}
+
 int createBurger(Product *p,int now){
     printf("이름은? ");
     scanf("%[^\n]s",p[now].name);
