30b60ed54ca7da7ca5417c6afe89278dfa8b86e4
2000kms <rlaalstkd0@naver.com>
2022-05-11 01:20:49 +0900

    [create] saveData

--- manager.c
+++ manager.c
@@ -1,1 +1,18 @@
+#include "manager.h"
+#include <stdio.h>
+#include <string.h>
+void saveData(Product *p,int now){      
+    FILE *fp;
+    fp=fopen("burger.txt","wt");
+        for(int i=0;i<now;i++){
+        if(p[i].price==-1){
+            continue;
+        }
+        else if(p[i].price!=-1){
+        fprintf(fp,"%s\t%d\t%s\t%s\t%s\n",p[i].name,p[i].price,p[i].bread,p[i].patty,p[i].sauce);
+        }
+    }
+    fclose(fp);
+    return ;
+}
 
