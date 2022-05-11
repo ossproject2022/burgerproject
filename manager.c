2ba4b20b139f1b5039dac4ae7c6763b3db56ee58
sanha33 <mountrive@naver.com>
2022-05-12 01:31:13 +0900

    [create] LoadData()

--- manager.c
+++ manager.c
@@ -16,3 +16,33 @@ void saveData(Product *p,int now){
     return ;
 }
 
+int loadData(Product *p){
+    char buffer[20];
+int i;
+FILE *fp;
+fp=fopen("burger.txt","rt");
+if(fp==NULL){
+    printf("파일 없음\n");
+    return 0;
+ }  else{
+    for(i=0;i<20;i++){
+        if(i!=0){
+            fgets(buffer,sizeof(buffer),fp);
+        }
+        fscanf(fp,"%[^\t]s",p[i].name);
+        if(feof(fp)){
+            
+            break;
+        }
+        fscanf(fp," %d",&p[i].price);
+        fscanf(fp," %[^\t]s",p[i].bread);
+        fscanf(fp," %[^\t]s",p[i].patty);
+        fscanf(fp,"%[^\t]s",p[i].sauce);
+    }
+ }
+    fclose(fp);
+    if(i>0){
+    printf("=>로딩 성공!\n");
+    }
+    return i;
+}
