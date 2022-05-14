7214657fcaca280ee6f087a9b26c698f922e4756
sanha33 <mountrive@naver.com>
2022-05-14 22:27:09 +0900

    [update]

--- main.c
+++ main.c
@@ -4,41 +4,42 @@
 #include "manager.h"
 #include "product.h"
 
+
 int main(void){
 
-	Product *p=(Product *)malloc(sizeof(Product)*20);
-	int menu;
-	int now=0;
-
-	int result;
-	while (1){
-		menu = selectMenu();
-		if (menu == 0) {
-			printf("종료됨!\n");
-			break;
-		}
-		if (menu == 1){
-			result=listBurger(p,now);
-			if(result==0){
-				printf("데이터 없음\n");
-			}
-		}
-		else if (menu == 2){
-			now=createBurger(p,now);
-			if(now!=0)printf("=>추가됨!\n");
-		}
-		else if (menu == 3){
-			result=updateBurger(p,now);
-			if(result==1){
-				printf("=>수정됨!\n");
-			}
-		}
-		else if (menu == 4){
-			result=deleteBurger(p,now);
-			if(result==1){
-				printf("삭제됨!\n");
-			}
-		}
+    Product *p=(Product *)malloc(sizeof(Product)*20);
+    int menu;
+    int now=0;
+    now=loadData(p);
+    int result;
+    while (1){
+        menu = selectMenu();
+        if (menu == 0) {
+            printf("종료됨!\n");
+            break;
+        }
+        if (menu == 1){
+            result=listBurger(p,now);
+            if(result==0){
+                printf("데이터 없음\n");
+            }
+        }
+        else if (menu == 2){
+            now=createBurger(p,now);
+            if(now!=0)printf("=>추가됨!\n");
+        }
+        else if (menu == 3){
+            result=updateBurger(p,now);
+            if(result==1){
+                printf("=>수정됨!\n");
+            }
+        }
+        else if (menu == 4){
+            result=deleteBurger(p,now);
+            if(result==1){
+                printf("삭제됨!\n");
+            }
+        }
         else if(menu==5){
         saveData(p,now);
                 printf("저장됨!\n");
@@ -55,7 +56,14 @@ int main(void){
                 printf("검색된 데이터 없음!\n");
             }
         }
-	}
-	free(p);
-	return 0;
+    else if(menu==8){
+            result=searchBurger(p,now);
+            if(result==0){
+                printf("검색된 데이터 없음!\n");
+            }
+
+        }
+    }
+    free(p);
+    return 0;
 }
